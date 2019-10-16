a58851d1e8308c4484cffe5dfdfc0ca45efcb74b
Tianyuan Fu <ftygary@gmail.com>
2019-10-16 01:20:10 -0700

    added hm1; [WIP] test cases 1/5

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -4,24 +4,101 @@
 float nn_interpolate(image im, float x, float y, int c)
 {
     // TODO Fill in
-    return 0;
+    int new_x = round(x);
+    int new_y = round(y);
+    float value = get_pixel(im, new_x, new_y, c);
+    return value;
 }
 
 image nn_resize(image im, int w, int h)
 {
-    // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    image new_image = make_image(w, h, im.c);
+
+    for(int i = 0; i < im.c; i ++) {
+        for(int j = 0; j < h; j ++) {
+            for(int k = 0; k < w; k ++) {
+                // map to the original coordinate
+                // the beginning positon should be: -0.5 + a/2 (a is the ratio of original / new)
+                float a = (float)im.w / w;
+
+                float x = -0.5 + a / 2 + (float)k * a;
+                float y = -0.5 + a / 2 + (float)j * a;
+                
+                set_pixel(new_image, k, j, i, nn_interpolate(im, x, y, i));
+            }
+        }
+    }
+
+    return new_image;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
 {
-    // TODO
-    return 0;
+    int l, r, u, d;
+    l = (int)x;
+    r = l + 1;
+    u = (int) y;
+    d = u + 1;
+
+    float V1, V2, V3, V4;
+    V1 = get_pixel(im, l, u, c);
+    V2 = get_pixel(im, r, u, c);
+    V3 = get_pixel(im, l, d, c);
+    V4 = get_pixel(im, r, d, c);
+
+    float d1, d2, d3, d4;
+    d1 = x - l;
+    d2 = r - x;
+    d3 = y - u;
+    d4 = d - y;
+    
+    if(l < 0) {
+        V1 = 0;
+        V3 = 0;
+        d1 = 0;
+    }
+    if(r > im.w - 1) {
+        V2 = 0;
+        V4 = 0;
+        d2 = 0;
+    }
+    if(u < 0) {
+        V1 = 0;
+        V2 = 0;
+        d3 = 0;
+    }
+    if(d > im.h - 1) {
+        V3 = 0;
+        V4 = 0;
+        d4 = 0;
+    }
+
+    float q1, q2, q;
+    q1 = V1 * d2 + V2 * d1;
+    q2 = V3 * d2 + V4 * d1;
+    q = q1 * d4 + q2 * d3;
+
+    return q;
 }
 
 image bilinear_resize(image im, int w, int h)
 {
-    // TODO
-    return make_image(1,1,1);
+    image new_image = make_image(w, h, im.c);
+
+    for(int i = 0; i < im.c; i ++) {
+        for(int j = 0; j < h; j ++) {
+            for(int k = 0; k < w; k ++) {
+                // map to the original coordinate
+                // the beginning positon should be: -0.5 + a/2 (a is the ratio of original / new)
+                float a = (float)im.w / w;
+
+                float x = -0.5 + a / 2 + (float)k * a;
+                float y = -0.5 + a / 2 + (float)j * a;
+                
+                set_pixel(new_image, k, j, i, bilinear_interpolate(im, x, y, i));
+            }
+        }
+    }    
+    return new_image;
 }
 
